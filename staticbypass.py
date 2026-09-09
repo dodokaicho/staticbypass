ee5f5ac4c7f629c6c36bcf3ab6bf0b13f687033a
Bill Chenyoung <bchenyoung@gmail.com>
2026-09-09 16:38:09 +1000

    Fixed bugs in hellsgate

--- staticbypass.py
+++ staticbypass.py
@@ -28,7 +28,7 @@ def main() -> None:
     parser.add_argument('-l', "--language", type=str, choices={"c","cs","ps1","vba", "rs", "go", "pas", "nim"}, required=True, help='Language used to write and compile')
     parser.add_argument('-f', "--obfuscator", type=str, required=False, help='Obfuscators transform the transformed shellcode bytes into other formats, such as strings.')
     parser.add_argument('-g', "--guardrails", type=str, nargs='*', required=False, help='Guardrails stop execution if some condition is met.')
-    parser.add_argument('-b', "--preprocessors", type=str, nargs = '*', required=False, help='Preprocessors modify the shellcode but are self decoding.')
+    parser.add_argument('-b', "--preprocessors", type=str, nargs='*', required=False, help='Preprocessors modify the shellcode but are self decoding.')
     parser.add_argument('-a', "--postprocessors", type=str, nargs='*', required=False, help='Postprocessors obfuscate the resulting exe or script, e.g. packers')
     parser.add_argument('-d', "--delivery", type=str, required=False, default="embedded", help='Delivery defines where the obfuscated shellcode is retrieved')
     parser.add_argument('-o', "--output", type=str, required=False, default="output", help='Output file name')
